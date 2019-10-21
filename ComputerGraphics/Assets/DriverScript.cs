db748224677878c27a96669d30af4e44d870d9e7
T00137667 <T00137667@gmail.com>
2019-10-21 16:01:22 +0100

    Bresenhams Added
    Signed-off-by: T00137667 <T00137667@gmail.com>

--- ComputerGraphics/Assets/DriverScript.cs
+++ ComputerGraphics/Assets/DriverScript.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using Assets;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DriverScript : MonoBehaviour
 {
-    Vector2 pointA = new Vector2(0.5f, 0.7f);
-    Vector2 pointB = new Vector2(-0.2f, -0.5f);
+    Vector2 pointA = new Vector2(5.5f, 0.7f);
+    Vector2 pointB = new Vector2(13.2f, 7.5f);
 
     // Start is called before the first frame update
     void Start()
@@ -14,11 +15,27 @@ public class DriverScript : MonoBehaviour
         Outcode outcodeA = new Outcode(pointA);
         Outcode outcodeB = new Outcode(pointB);
         Outcode inViewPort = new Outcode();
+        List<Vector2Int> vector2Ints = new List<Vector2Int>();
+
+        int screenwidth = Screen.width;
+        int screenHeight = Screen.height;
 
         outcodeA.printOutcode();
         outcodeB.printOutcode();
 
-        lineClip(ref pointA, ref pointB);
+        LineClip.lineClip(ref pointA, ref pointB);
+        print(pointA + " " + pointB);
+
+        Vector2Int pAI = new Vector2Int((int)pointA.x, (int)pointA.y);
+        Vector2Int pBI = new Vector2Int((int)pointB.x, (int)pointB.y);
+
+        vector2Ints = Breshenhams(pAI, pBI);
+
+
+        foreach (Vector2Int v in vector2Ints)
+            print(v);
+
+        print(pointA + pointB);
 
         if ((outcodeA + outcodeB) == inViewPort)
         {
@@ -30,55 +47,77 @@ public class DriverScript : MonoBehaviour
         }
     }
 
-    public bool lineClip(ref Vector2 v, ref Vector2 u)
+    private List<Vector2Int> Breshenhams(Vector2Int v, Vector2Int u)
     {
-        Outcode outcodeV = new Outcode(v);
-        Outcode outcodeU = new Outcode(u);
-        Outcode inViewPort = new Outcode();
+        int diffX = (int)(u.x - v.x);
+        if (diffX < 0) return Breshenhams(u, v);
 
-        if ((outcodeV + outcodeU) == inViewPort) return true;
-        if ((outcodeV * outcodeU) == inViewPort) return false;
+        int diffY = (int)(u.y - v.y);
+        if (diffY < 0) negateY(Breshenhams(negateY(v), negateY(u)));
 
-        if (outcodeV == inViewPort)
-            lineClip(ref u, ref v);
+        if (diffY > diffX) swapXY(Breshenhams(swapXY(v), swapXY(u)));
+
+        int twoDY = diffY * 2;
+        int TwoDY2DX = twoDY - (diffX * 2);
+        int p = twoDY - diffY;
+        List<Vector2Int> points = new List<Vector2Int>();
+        points.Add(v);
 
 
 
-        if (outcodeV.up)  // Above viewport
+        for (int i = (int)v.x; i < u.x; i++)
         {
-           Vector2 newV =  intercept(v, u, 0);
+            
+            if (p > 0)
+            {
+                v.x++;
+                v.y++;
+                p += TwoDY2DX;
+            }
+            else
+            {
+                v.x++;
+                p += twoDY;
+            }
 
-            Outcode newVOutcode = new Outcode(newV);
-            if (newVOutcode == inViewPort)
-            { v = newV;
-                return lineClip(ref v, ref u);
+            points.Add(v);
         }
 
-        return false;
+        return points;
     }
 
-    private Vector2 intercept(Vector2 v, Vector2 u, int edgeId)
+    private Vector2Int swapXY(Vector2Int v)
     {
-        float slope = (u.y - v.y) / (u.x - v.x);
-
-        switch (edgeId)
-        {
-            case 0: //Top
-                return new Vector2( v.x + (1/slope)  * ( 1 - v.y) ,  1);
-
-            case 1: //Bottom
-                return new Vector2(v.x + (1 / slope) * (-1 - v.y), -1);
-
-            case 2: //Left
-                return new Vector2(-1, v.y + slope * (-1 - v.x));
-
-            default:  // Right
-
-            return new Vector2(1, v.y + slope * (1 - v.x));
-
-        }
+        return new Vector2Int(v.y, v.x);
     }
 
+    private List<Vector2Int> swapXY(List<Vector2Int> list)
+    {
+        List<Vector2Int> output_list = new List<Vector2Int>();
+
+        foreach (Vector2Int v in list)
+            output_list.Add(swapXY(v));
+
+
+        return output_list;
+    }
+
+
+    private List<Vector2Int> negateY(List<Vector2Int> list)
+    {
+        List<Vector2Int> output_list = new List<Vector2Int>();
+
+        foreach (Vector2Int v in list)
+            output_list.Add(negateY(v));
+
+
+        return output_list;
+    }
+
+    private Vector2Int negateY(Vector2Int v)
+    {
+        return new Vector2Int(v.x, -v.y);
+    }
 
 
 
--- ComputerGraphics/Assets/LineClip.cs
+++ ComputerGraphics/Assets/LineClip.cs
@@ -3,11 +3,97 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets
 {
     public class LineClip
     {
+        public static bool lineClip(ref Vector2 v, ref Vector2 u)
+        {
+            Outcode outcodeV = new Outcode(v);
+            Outcode outcodeU = new Outcode(u);
+            Outcode inViewPort = new Outcode();
 
+            if ((outcodeV + outcodeU) == inViewPort) return true;
+            if ((outcodeV * outcodeU) == inViewPort) return false;
+
+            if (outcodeV == inViewPort)
+                lineClip(ref u, ref v);
+
+
+
+            if (outcodeV.up)  // Above viewport
+            {
+                Vector2 newV = intercept(v, u, 0);
+
+                Outcode newVOutcode = new Outcode(newV);
+                if (newVOutcode == inViewPort)
+                {
+                    v = newV;
+                    return lineClip(ref v, ref u);
+                }
+            }
+
+            if (outcodeV.down)  // Below viewport
+            {
+                Vector2 newV = intercept(v, u, 1);
+
+                Outcode newVOutcode = new Outcode(newV);
+                if (newVOutcode == inViewPort)
+                {
+                    v = newV;
+                    return lineClip(ref v, ref u);
+                }
+            }
+
+            if (outcodeV.left)  // Left of viewport
+            {
+                Vector2 newV = intercept(v, u, 2);
+
+                Outcode newVOutcode = new Outcode(newV);
+                if (newVOutcode == inViewPort)
+                {
+                    v = newV;
+                    return lineClip(ref v, ref u);
+                }
+            }
+
+            if (outcodeV.right)  // Right of viewport
+            {
+                Vector2 newV = intercept(v, u, 3);
+
+                Outcode newVOutcode = new Outcode(newV);
+                if (newVOutcode == inViewPort)
+                {
+                    v = newV;
+                    return lineClip(ref v, ref u);
+                }
+            }
+
+            return false;
+        }
+
+        private static Vector2 intercept(Vector2 v, Vector2 u, int edgeId)
+        {
+            float slope = (u.y - v.y) / (u.x - v.x);
+
+            switch (edgeId)
+            {
+                case 0: //Top
+                    return new Vector2(v.x + (1 / slope) * (1 - v.y), 1);
+
+                case 1: //Bottom
+                    return new Vector2(v.x + (1 / slope) * (-1 - v.y), -1);
+
+                case 2: //Left
+                    return new Vector2(-1, v.y + slope * (-1 - v.x));
+
+                default:  // Right
+
+                    return new Vector2(1, v.y + slope * (1 - v.x));
+
+            }
+        }
     }
 }
